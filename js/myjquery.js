cca5aa943de6de4e086c1f15e42e3d3fa4a4e66a
picpoint <picpoint@yandex.ru>
2017-08-18 17:55:28 +0300

    делаем кнопку

---
+++ js/myjquery.js
@@ -0,0 +1,22 @@
+	$(document).ready(function(){
+
+/*	
+		var main = $('.main');
+		main.click(function() {
+			main.addClass('blue');
+		});
+		main.dblclick(function(){
+			main.removeClass('blue');
+		});
+	
+
+	var modal = $('.modal-link');
+		modal.click(function(){
+			console.log(modal.attr('href'));
+		});
+	*/
+
+
+	
+
+	});
