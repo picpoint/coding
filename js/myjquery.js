da60665a80abbbdbd5eb00fc4c07b63479bf7b7a
rmtar <picpoint@yandex.ru>
2017-08-22 00:11:18 +0300

    делаем ul меню

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,25 +1,11 @@
 	$(document).ready(function(){
 
-/*	
-		var main = $('.main');
-		main.click(function() {
-			main.addClass('blue');
-		});
-		main.dblclick(function(){
-			main.removeClass('blue');
-		});
-	
-
-	var modal = $('.modal-link');
-		modal.click(function(){
-			console.log(modal.attr('href'));
-		});
-	*/
-
 	var main = $('.main-link');
 		main.click(function(){
 			main.toggleClass('main-link-active');
 		});
 	
+	
+
 
 	});
