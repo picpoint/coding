ab39498e9c57bc342408186d261055032b868818
picpoint <picpoint@yandex.ru>
2017-08-22 17:50:12 +0300

    доделываю и отлаживаю скрипты

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,11 +1,29 @@
 	$(document).ready(function(){
 
 	var main = $('.main-link');
+	var menu = $('.main-menu');
+	var link = $('.main-menu a');
+		
 		main.click(function(){
 			main.toggleClass('main-link-active');
+			menu.toggleClass('main-menu-active');
 		});
-	
-	
 
+		link.click(function(){
+			link.toggleClass('main-menu');
+		});
+		
+	
 
 	});
+
+	/*
+
+.main - главный блок, родитель
+.main-link - кружок для клика вызова
+.main-link-active - кружек для клика АКТИВНЫЙ
+.main-menu - фиолетовое меню, по умолчанию не активное
+.main-menu-active - фиолетовое меню активное
+
+
+	*/
