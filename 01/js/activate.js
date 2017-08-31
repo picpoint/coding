740ba85ffd4439ab0d3ca9ec33c4f17325d284b2
rmtar <picpoint@yandex.ru>
2017-09-01 00:21:54 +0300

    Закончил 2ю часть паралаксов

--- 01/js/activate.js
+++
@@ -1,5 +0,0 @@
-var scene = document.getElementById('scene');
-// or, if you use jQuery
-var scene = $('#scene').get(0);
-
-var parallax = new Parallax(scene);
--- 01/js/test.js
+++ 01/js/test.js
@@ -1,1 +1,25 @@
-alert('HelloW');
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene, {
+  relativeInput: true,
+  clipRelativeInput: false,
+  hoverOnly: true,
+  inputElement: document.getElementById('myinput'),
+  calibrateX: false,
+  calibrateY: true,
+  invertX: false,
+  invertY: true,
+  limitX: false,
+  limitY: 10,
+  scalarX: 2,
+  scalarY: 8,
+  frictionX: 0.2,
+  frictionY: 0.8,
+  originX: 0.0,
+  originY: 1.0,
+  precision: 1,
+  pointerEvents: false,
+  onReady: function() { alert('ready!'); }
+});
+
+
+
